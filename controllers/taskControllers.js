0d77e1f7043ce9af0269e95bcce3e5ce7104ba9e
DevAdedeji <adedejitewogbade2@gmail.com>
2022-12-07 13:54:25 +0100

    Done with my first CRUD API built with NodeJS

--- controllers/taskControllers.js
+++ controllers/taskControllers.js
@@ -27,7 +27,49 @@ const saveTask = (req,res)=>{
 
 }
 
+const getATask = (req,res) =>{
+    const id = req.params.id
+    Task.findById(id)
+    .then(result=>{
+        if(result === null){
+            res.status(400).json({error:'Task Not Found'})
+        }
+        else{
+            res.status(200).json({data:result, success:true})
+        }
+        
+    })
+    .catch(err=>{
+        res.status(400).json({error:err.message})
+    })
+}
+
+const deleteATask = (req,res) =>{
+    const id = req.params.id
+    Task.findByIdAndDelete(id)
+    .then(result=>{
+        res.status(200).json({message:'Task deleted successfully', success:true})
+    })
+    .catch(err=>{
+        res.status(400).json({error:err.message})
+    })
+}
+
+const updateATask = (req,res) =>{
+    const id = req.params.id
+    Task.findByIdAndUpdate(id, req.body, {new:true})
+    .then(result=>{
+        res.status(200).json({data:result, success:true, message:'Task updated successfully', })
+    })
+    .catch(err=>{
+        res.status(400).json({error:err.message})
+    })
+}
+
 module.exports = {
     getAllTasks,
     saveTask,
+    getATask,
+    deleteATask,
+    updateATask
 }
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const taskController = require('../controllers/taskControllers')
 
 router.get('/', taskController.getAllTasks)
-router.post('/post', taskController.saveTask);
+router.post('/task', taskController.saveTask);
+router.get('/task/:id', taskController.getATask)
+router.delete('/task/:id', taskController.deleteATask)
+router.put('/task/:id', taskController.updateATask)
+
 
 module.exports = router;
